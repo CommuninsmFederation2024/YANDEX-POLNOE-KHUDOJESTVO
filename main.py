32902887313b7c51e77098a48cdd412d4d2abb7f
CommunismFederation2025 <khlvr2009@gmail.com>
2025-02-17 17:58:38 +0600

    Create main.py
    ВАШ ДОЛЯ НАЧАЛЬНИКА

---
+++ main.py
@@ -0,0 +1,7 @@
+# приуэт началника ми усе делали кака ты сказати. тоби ми уси ради
+# пасиба начальника. храни тебе господь
+import sys
+from PyQt6 import uic
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+import requests
